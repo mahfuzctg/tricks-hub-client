53aeb2bf81d3eaea982c01bbccb072fd61302a56
Mahfuz <aamahfuz.pro@gmail.com>
2024-10-07 16:43:07 +0600

    added layout

---
+++ src/app/looding.tsx
@@ -0,0 +1,10 @@
+
+const loading = () => {
+    return (
+        <div className="w-screen h-screen bg-gray-500 flex justify-center items-center animate-pulse">
+            
+        </div>
+    );
+};
+
+export default loading;
