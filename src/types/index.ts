83f013376f2b4092f96a53ce37f9626408f0088d
Mahfuz <aamahfuz.pro@gmail.com>
2024-11-08 22:14:34 +0600

    added and modified something

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,3 +6,20 @@ export type TJwtDecoded = {
   iat : number;
   role : string;
 }
+
+
+// Generic API Response Type
+export interface ApiResponse<T = any> {
+  success: boolean;
+  data: T;
+}
+
+// User Type (TUser)
+export interface TUser {
+  _id: string;
+  email: string;
+  name: string;
+  role: string;
+  image: string;
+  followers?: string[];
+}
